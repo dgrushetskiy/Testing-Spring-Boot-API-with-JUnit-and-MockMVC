3f7390340e7c6d4624919513fc73d1e80efad32e
Numero Uno <numerounodeveloper@gmail.com>
2022-12-21 11:56:41 -0500

    Initial commit

---
+++ src/test/java/com/example/demo/RESTCallTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class RESTCallTest {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldReturnStudent() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/1",
+                String.class)).contains("Ana");
+    }
+}
